182d114aca96788bbe18d3dc74bb3eca0e731adf
Colton Donnelly <donnelly.colton@gmail.com>
2018-02-10 06:56:30 -0500

    trying to make R work lol

--- app/src/main/java/com/coolkids/todo/todoapp/MainActivity.kt
+++ app/src/main/java/com/coolkids/todo/todoapp/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     //  the LoginActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
         if (hasCredentials())
             goToLoginActivity()
         else
