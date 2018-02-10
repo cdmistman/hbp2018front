5f2c5b60ee24b47b046ce09fb1c2b5e2ecebaa0a
Colton Donnelly <donnelly.colton@gmail.com>
2018-02-10 00:27:29 -0500

    attempt to make things work for once

--- app/src/main/java/com/coolkids/todo/todoapp/MainActivity.kt
+++ app/src/main/java/com/coolkids/todo/todoapp/MainActivity.kt
@@ -5,8 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {test
         setContentView(R.layout.activity_main)
     }
 }
