4e1f4b533c1984523bf738855b3c1d2eb50a76cb
Colton Donnelly <donnelly.colton@gmail.com>
2018-02-10 10:15:18 -0500

    started adding handlers and objects and stuff cause these are just a few of my favorite things

---
+++ app/src/main/java/com/coolkids/todo/todoapp/PlannedEvent.kt
@@ -0,0 +1,10 @@
+package com.coolkids.todo.todoapp
+
+import org.json.JSONObject
+
+/**
+ * Created by colton on 2/10/18.
+ */
+object PlannedEvent(event: JSONObject) {
+
+}
---
+++ app/src/main/java/com/coolkids/todo/todoapp/ServerHandler.java
@@ -0,0 +1,20 @@
+package com.coolkids.todo.todoapp;
+
+
+import org.json.JSONObject;
+
+/**
+ * Handles the interactions with the server
+ */
+
+public class ServerHandler {
+    String server;
+    int port;
+
+    public PlannedEvent
+}
+
+
+// httpserver thing .get(address, params)
+// post requests
+// put requests
