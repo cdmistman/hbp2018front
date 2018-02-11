5275201bc75b20269f82d1f5631ae1121038f6fc
Colton Donnelly <donnelly.colton@gmail.com>
2018-02-11 08:16:54 -0500

    some stuff

---
+++ app/src/main/java/com/coolkids/todo/getTogether/EventActivity.kt
@@ -0,0 +1,12 @@
+package com.coolkids.todo.getTogether
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class EventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event)
+    }
+}
