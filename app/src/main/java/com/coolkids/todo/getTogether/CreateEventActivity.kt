ca14e82a57b76871912127c923a037a0ad0551fe
Colton Donnelly <donnelly.colton@gmail.com>
2018-02-11 07:57:17 -0500

    Finished CreateTaskActivity
    need to undo the TODO() for the createTask function once EventActivity is finished

--- app/src/main/java/com/coolkids/todo/getTogether/CreateEventActivity.kt
+++ app/src/main/java/com/coolkids/todo/getTogether/CreateEventActivity.kt
@@ -26,8 +26,8 @@ class CreateEventActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_createevent)
 
-        eventNameField = findViewById(R.id.event_name)
-        eventDescriptionField = findViewById(R.id.event_description)
+        eventNameField = findViewById(R.id.create_task_event_name)
+        eventDescriptionField = findViewById(R.id.create_task_title)
         eventDateField = findViewById(R.id.event_date)
         eventTimeField = findViewById(R.id.event_time)
         eventLocationField = findViewById(R.id.event_location)
